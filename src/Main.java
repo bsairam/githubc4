14561e7c9eb732206793b83e16d39ff8bbbf72de
bsairam <32007825+bsairam@users.noreply.github.com>
2017-09-16 12:15:31 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//You may test that your code works find here
+		//Please check that your code works and has no
+		//compilation problems before to submit
+		Student p = new Student(01,"ram",new Date(1995,11,22),62.78);
+		Student p1 = new Student(02,"rani",new Date(1997,01,01),63.56);
+		Student p2 = new Student(03,"shiva",new Date(1996,05,06),74.12);
+		Student p3 = new Student(04,"lambad",new Date(1996,11,06),68.41);
+
+      /*  StudentGroup sg=new StudentGroup(10);
+
+	Date d=new Date(1996,11,20);
+	Student s1=new Student(22,"xyz",d,82.0);
+
+                sg.addFirst(s1);
+
+              System.out.println(sg.getStudent(0).getId());
+*/
+
+	}
+
+}
